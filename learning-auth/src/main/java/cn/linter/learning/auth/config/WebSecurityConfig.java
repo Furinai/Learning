33ea6f6e9867da9836199556197a152a2a6fc0ac
wangxiaoyang <15847424609@163.com>
2021-02-09 23:36:51 +0800

    refactor: 认证服务器依赖改用spring security oauth2

--- learning-auth/src/main/java/cn/linter/learning/auth/config/WebSecurityConfig.java
+++ learning-auth/src/main/java/cn/linter/learning/auth/config/WebSecurityConfig.java
@@ -3,10 +3,8 @@ package cn.linter.learning.auth.config;
 import cn.linter.learning.auth.client.UserClient;
 import cn.linter.learning.auth.entity.User;
 import cn.linter.learning.common.entity.ResultStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,9 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-
-import java.security.KeyPair;
 
 /**
  * Security配置
@@ -31,21 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserClient userClient;
 
-    @Value("#{'${security.oauth2.authorization.jwt.key-store}'.substring(10)}")
-    private String keyLocation;
-    @Value("${security.oauth2.authorization.jwt.key-store-password}")
-    private String keyPassword;
-    @Value("${security.oauth2.authorization.jwt.key-alias}")
-    private String keyAlias;
-
     public WebSecurityConfig(UserClient userClient) {
         this.userClient = userClient;
-    }
-
-    @Bean
-    public KeyPair keyPairFactory() {
-        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(keyLocation), keyPassword.toCharArray());
-        return keyStoreKeyFactory.getKeyPair(keyAlias, keyPassword.toCharArray());
     }
 
     @Bean
