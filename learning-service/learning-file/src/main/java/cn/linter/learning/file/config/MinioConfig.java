6b15ecf60f545ffef8aa0c12e7d2a6ce6ea69e2f
wangxiaoyang <15847424609@163.com>
2021-03-14 13:34:28 +0800

    feat: 新增文件服务

---
+++ learning-service/learning-file/src/main/java/cn/linter/learning/file/config/MinioConfig.java
@@ -0,0 +1,30 @@
+package cn.linter.learning.file.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Minio配置
+ *
+ * @author wangxiaoyang
+ * @date 2021/3/14
+ */
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String endPoint;
+    @Value("${minio.access-key}")
+    private String accessKey;
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClientFactory() {
+        return MinioClient.builder().endpoint(endPoint)
+                .credentials(accessKey, secretKey).build();
+    }
+
+}
---
+++ learning-service/learning-file/src/test/java/FileApplicationTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 文件服务测试类
+ *
+ * @author wangxiaoyang
+ * @since 2020/11/12
+ */
+@SpringBootTest
+public class FileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
