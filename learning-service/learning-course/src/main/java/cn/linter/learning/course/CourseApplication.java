32271131d2e79ee3b2cb3b925ebfbfdbd06651ef
citrucn <1948288473@qq.com>
2021-05-26 09:52:23 +0800

    refactor: 替换废弃的注解

--- learning-service/learning-course/src/main/java/cn/linter/learning/course/CourseApplication.java
+++ learning-service/learning-course/src/main/java/cn/linter/learning/course/CourseApplication.java
@@ -1,7 +1,8 @@
 package cn.linter.learning.course;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 课程服务启动类
@@ -9,7 +10,8 @@ import org.springframework.cloud.client.SpringCloudApplication;
  * @author wangxiaoyang
  * @since 2021/02/16
  */
-@SpringCloudApplication
+@SpringBootApplication
+@EnableDiscoveryClient
 public class CourseApplication {
 
     public static void main(String[] args) {
