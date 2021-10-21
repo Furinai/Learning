ac51f64b8dfd73c43ba721d020f71d583a1e6a8e
wangxiaoyang <1948288473@qq.com>
2021-10-21 21:35:59 +0800

    :memo: 完善数据库DDL文件

--- learning.sql
+++ learning.sql
@@ -1,192 +1,159 @@
-/*
- Navicat Premium Data Transfer
+create table answer
+(
+    id bigint auto_increment comment '答案ID'
+        primary key,
+    content varchar(1000) not null comment '内容',
+    user_name varchar(20) not null comment '用户名',
+    question_id bigint not null comment '问题ID',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '修改时间'
+)
+    comment '答案';
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 80022
- Source Host           : localhost:3306
- Source Schema         : learning
+create table category
+(
+    id int auto_increment comment '分类ID'
+        primary key,
+    name varchar(20) not null comment '名称',
+    parent_id int not null comment '父级ID',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '修改时间'
+)
+    comment '分类';
 
- Target Server Type    : MySQL
- Target Server Version : 80022
- File Encoding         : 65001
+create table category_course
+(
+    id bigint auto_increment comment '分类-课程关系ID'
+        primary key,
+    category_id int not null comment '分类ID',
+    course_id bigint not null comment '课程ID'
+);
 
- Date: 18/03/2021 00:03:29
-*/
+create table chapter
+(
+    id bigint auto_increment comment '章节ID'
+        primary key,
+    title varchar(100) not null comment '标题',
+    type varchar(10) not null comment '内容类型',
+    video_url varchar(255) null comment '视频链接',
+    video_time varchar(20) null comment '视频时长',
+    text_content text null comment '文字内容',
+    course_id bigint not null comment '课程ID',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '修改时间'
+)
+    comment '章节';
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+create table course
+(
+    id bigint auto_increment comment '课程ID'
+        primary key,
+    name varchar(100) not null comment '名称',
+    price decimal(10,2) not null comment '价格',
+    description varchar(200) not null comment '简介',
+    user_name varchar(20) not null comment '用户名',
+    average_score tinyint not null comment '平均评分',
+    cover_picture varchar(100) not null comment '封面图片地址',
+    approved tinyint not null comment '审核通过',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '修改时间'
+)
+    comment '课程';
 
--- ----------------------------
--- Table structure for answer
--- ----------------------------
-DROP TABLE IF EXISTS `answer`;
-CREATE TABLE `answer`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '答案ID',
-  `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
-  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `question_id` bigint(0) NOT NULL COMMENT '问题ID',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '答案' ROW_FORMAT = Dynamic;
+create table evaluation
+(
+    id bigint auto_increment comment '评价ID'
+        primary key,
+    score tinyint not null comment '评分',
+    comment varchar(255) not null comment '评论',
+    user_name varchar(20) not null comment '用户名',
+    course_id bigint not null comment '课程ID',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '修改时间'
+)
+    comment '评价';
 
--- ----------------------------
--- Table structure for category
--- ----------------------------
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '分类ID',
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
-  `parent_id` bigint(0) NOT NULL COMMENT '父级ID',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分类' ROW_FORMAT = Dynamic;
+create table note
+(
+    id bigint auto_increment comment '笔记ID'
+        primary key,
+    content text not null comment '内容',
+    user_name varchar(20) not null comment '用户名',
+    course_id bigint not null comment '课程ID',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '修改时间'
+)
+    comment '笔记';
 
--- ----------------------------
--- Table structure for category_course
--- ----------------------------
-DROP TABLE IF EXISTS `category_course`;
-CREATE TABLE `category_course`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '分类-课程关系ID',
-  `category_id` int(0) NOT NULL COMMENT '分类ID',
-  `course_id` bigint(0) NOT NULL COMMENT '课程ID',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+create table `order`
+(
+    id bigint auto_increment
+        primary key,
+    trade_no varchar(40) not null comment '交易编号',
+    price decimal(10,2) not null comment '价格',
+    user_name varchar(255) not null comment '买家',
+    product_id bigint not null comment '商品ID',
+    product_name varchar(255) not null comment '商品名称',
+    close_milliseconds bigint not null comment '关闭时间',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '更新时间',
+    status tinyint not null comment '状态：0->未支付 1->已支付 2->已关闭'
+)
+    comment '订单';
 
--- ----------------------------
--- Table structure for chapter
--- ----------------------------
-DROP TABLE IF EXISTS `chapter`;
-CREATE TABLE `chapter`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '章节ID',
-  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
-  `type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容类型',
-  `video_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '视频链接',
-  `video_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '视频时长',
-  `text_content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文字内容',
-  `course_id` bigint(0) NOT NULL COMMENT '课程ID',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '章节' ROW_FORMAT = Dynamic;
+create table question
+(
+    id bigint auto_increment comment '问题ID'
+        primary key,
+    title varchar(255) not null comment '标题',
+    content varchar(1000) not null comment '内容',
+    user_name varchar(20) not null comment '用户名',
+    course_id bigint not null comment '课程ID',
+    answer_count int not null comment '答案数量',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '更新时间'
+)
+    comment '问题';
 
--- ----------------------------
--- Table structure for course
--- ----------------------------
-DROP TABLE IF EXISTS `course`;
-CREATE TABLE `course`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '课程ID',
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
-  `price` decimal(10, 2) NOT NULL COMMENT '价格',
-  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '简介',
-  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `average_score` tinyint(0) NOT NULL COMMENT '平均评分',
-  `cover_picture` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '封面图片地址',
-  `approved` tinyint(0) NOT NULL COMMENT '审核通过',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '课程' ROW_FORMAT = Dynamic;
+create table role
+(
+    id int auto_increment comment '主键ID'
+        primary key,
+    name varchar(20) not null comment '角色名',
+    create_time datetime not null comment '创建时间',
+    update_time datetime null comment '更新时间'
+)
+    comment '角色';
 
--- ----------------------------
--- Table structure for evaluation
--- ----------------------------
-DROP TABLE IF EXISTS `evaluation`;
-CREATE TABLE `evaluation`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '评价ID',
-  `score` tinyint(0) NOT NULL COMMENT '评分',
-  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论',
-  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `course_id` bigint(0) NOT NULL COMMENT '课程ID',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '评价' ROW_FORMAT = Dynamic;
+create table user
+(
+    id bigint auto_increment comment '主键ID'
+        primary key,
+    username varchar(20) not null comment '用户名',
+    password varchar(100) not null comment '密码',
+    full_name varchar(20) null comment '姓名',
+    gender char(2) null comment '性别',
+    role_id int not null comment '角色ID',
+    email_address varchar(100) not null comment '邮箱地址',
+    phone_number varchar(100) null comment '手机号码',
+    profile_picture varchar(100) null comment '头像链接',
+    create_time datetime not null comment '创建时间',
+    update_time datetime not null comment '修改时间'
+)
+    comment '用户';
 
--- ----------------------------
--- Table structure for note
--- ----------------------------
-DROP TABLE IF EXISTS `note`;
-CREATE TABLE `note`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '笔记ID',
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
-  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `course_id` bigint(0) NOT NULL COMMENT '课程ID',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '笔记' ROW_FORMAT = Dynamic;
+create table user_chapter
+(
+    id bigint auto_increment comment '用户-章节关系ID'
+        primary key,
+    user_name varchar(20) not null comment '用户名',
+    chapter_id bigint not null comment '章节ID'
+);
 
--- ----------------------------
--- Table structure for question
--- ----------------------------
-DROP TABLE IF EXISTS `question`;
-CREATE TABLE `question`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '问题ID',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
-  `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
-  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `course_id` bigint(0) NOT NULL COMMENT '课程ID',
-  `answer_count` int(0) NOT NULL COMMENT '答案数量',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '问题' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for role
--- ----------------------------
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of role
--- ----------------------------
-INSERT INTO `role` VALUES (1, '学生', '2021-01-26 14:46:24', '2021-01-26 14:46:26');
-INSERT INTO `role` VALUES (2, '教师', '2021-03-14 13:46:10', '2021-03-14 13:46:12');
-INSERT INTO `role` VALUES (3, '管理员', '2021-03-14 13:46:21', '2021-03-14 13:46:24');
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
-  `full_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '姓名',
-  `gender` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别',
-  `role_id` int(0) NOT NULL COMMENT '角色ID',
-  `email_address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '邮箱地址',
-  `phone_number` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
-  `profile_picture` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '头像链接',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '$2a$10$YmZg6KFPsMVUyWzsxiNgOun7SYoKW1FiAqFvaqnehx2fj0xh5geSa', '张三', '男', 2, 'admin@qq.com', '15648768456', 'http://localhost:8080/dfs/profile-pictures/default.jpg', '2021-01-26 14:36:52', '2021-03-16 19:53:49');
-
--- ----------------------------
--- Table structure for user_course
--- ----------------------------
-DROP TABLE IF EXISTS `user_course`;
-CREATE TABLE `user_course`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '用户-课程关系ID',
-  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `course_id` bigint(0) NOT NULL COMMENT '课程ID',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
-SET FOREIGN_KEY_CHECKS = 1;
+create table user_course
+(
+    id bigint auto_increment comment '用户-课程关系ID'
+        primary key,
+    user_name varchar(20) not null comment '用户名',
+    course_id bigint not null comment '课程ID'
+);
